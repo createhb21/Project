32d01c7df4b84690a3870b8ee07c02092d44a28e
Hyobumlee <alsghk9701@naver.com>
2021-05-18 04:18:03 +0900

    5워 17일 커밋 오늘도 열심히

--- learnJS with J/Advanced JavaScript/chapter1.js
+++ learnJS with J/Advanced JavaScript/chapter1.js
@@ -116,7 +116,7 @@ function myFn() {
 }
 
 console.log(myFn()) // call a function
- 
+
 
 let person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" }
 
@@ -457,7 +457,7 @@ pow(2, 3) = 8
 pow(2, 4) = 16
 
 
-// iterative
+// iterative 반복될 수 있는
 function pow(x, n) {
   let result = 1;
 
@@ -471,7 +471,7 @@ function pow(x, n) {
 
 console.log(pow(2, 3)); // 8
 
-// Recursive
+// Recursive 자기가 자기를 호출    ==> 메모리 과부하 가능성 high
 function pow(x, n) {
   if (n == 1) {
       return x;
@@ -488,6 +488,8 @@ pow(2, 3) = 2 * pow(2, 2)
 pow(2, 2) = 2 * pow(2, 1)
 pow(2, 1) = 2
 
+
+
 // ternary operator
 function pow(x, n) {
   return (n == 1) ? x : (x * pow(x, n - 1));
@@ -495,8 +497,15 @@ function pow(x, n) {
 
 // Haskell
 
-// The execution stack
-*/
+//              if n==1  = x
+//             /
+/ /pow(x, n) =
+//             \
+//              else     = x * pow(x, n - 1)
+
+// The execution stack => 메모리에 차곡차곡 저장
+하나의 function이 하나의 stack data를 구성한다.
+*/ 
 
 
 /* The execution stack = context = environment
@@ -519,7 +528,7 @@ console.log(pow(2, 3));
 */
 
 /* Recursive traversals
-function pow(x, n) { // only one execution stack is made. 
+function pow(x, n) { // iterative 방식... => only one execution stack is made. 
   let result = 1;
 
   // multiply result by x n times in the loop
@@ -531,12 +540,29 @@ function pow(x, n) { // only one execution stack is made.
 }
 
 alert(pow(2, 3)); // 8
- 
-let company = { // the same object, compressed for brevity
-  sales: [{ name: 'John', salary: 1000 }, { name: 'Alice', salary: 600 }],
+
+let company = { // the same object, compressed for brevity (서로 다른 구조를 갖고 있는 property, methods들일 때 iteration 사용할 수 없음. recursion만 쓸 수 있다는..)
+  sales: [{ 
+        name: 'John', 
+        salary: 1000 
+      }, { 
+        name: 'Alice', 
+        salary: 600 
+      }],
+
   development: {
-      sites: [{ name: 'Peter', salary: 2000 }, { name: 'Alex', salary: 1800 }],
-      internals: [{ name: 'Jack', salary: 1300 }]
+      sites: [{ 
+          name: 'Peter',
+          salary: 2000 
+      }, { 
+          name: 'Alex',
+          salary: 1800 
+      }],
+
+  internals: [{ 
+        name: 'Jack', 
+        salary: 1300 
+      }]
   }
 };
 
@@ -553,11 +579,12 @@ function sumSalaries(department) {
   }
 }
 
-alert(sumSalaries(company)); // 6700
+console.log(sumSalaries(company)); // 6700
+
 */
 
-/* JS 72 - Recursive structures
-// Linked list
+/* JS 72 - Recursive structures 을 활용해서
+// Linked list를 만들어 낼 수 있다능... 
 let arr = [obj2, obj2_1, obj3, obj1000];
 // pop, push, shift, unshift
 
@@ -607,14 +634,38 @@ console.log(list)   // {value: "new item", next: {value: 1, next: {value: 2, nex
 // sumTo(100) = 100 + 99 + ... + 2 + 1 = 5050
 
 // 1. Using a for loop. Iteration
-// 2. Using a recursion, cause sumTo(n) = n + sumTo(n-1) for n > 1.
+// 2. Using a recursion, cause sumTo(n) = n + sumTo(n-1) for n > 1
 // 3. Using the arithmetic progression formula.
 
 function sumTo(n) {  }
 
-alert( sumTo(10000) ); // 5050
+alert( sumTo(100) ); // 5050
+
+1. 반복문 사용하기:
+function sumTo(n) {
+    let sum = 0;
+    for (let i = 0; i < n+1; i++) {    
+        sum += i;
+    }
+    return sum;
+};
+
+sumTo(100); // 5050
+
+2. 재귀 사용하기:
+function sumTo(n) {
+  if (n == 1) return 1;
+  return n + sumTo(n - 1);
+}
+
+alert( sumTo(100) );
 
 
+더 생각해보기 1: 등차수열의 합공식을 사용하는 방법이 가장 빠릅니다. n에 관계없이 오직 세 개의 연산만 수행하면 되니까요. 수학은 항상 뭔가에 도움을 줍니다!
+
+반복을 사용하는 방법은 두 번째로 빠릅니다. 재귀를 사용하는 방법과 반복문을 사용하는 방법 모두 같은 수의 숫자를 더하는 것에서 같지만, 재귀를 사용하는 방법은 중첩 호출과 실행 스택 관리가 추가로 필요하기 때문에 더 많은 자원을 소비합니다. 따라서 속도가 더 느리죠.
+
+더 생각해보기 2: 몇몇 자바스크립트 엔진은 ‘tail call’ 최적화를 지원합니다. 위 함수 sumTo처럼 함수가 가장 마지막으로 수행하는 연산이 재귀 호출이라면 외부 함수는 실행을 다시 시작할 필요가 없기 때문에 엔진은 실행 컨텍스트를 기억할 필요가 없어집니다. 메모리 부담이 사라지는 거죠. 그렇기 때문에 sumTo(100000)같은 계산이 가능한 것입니다. 그런데 자바스크립트 엔진이 tail call 최적화를 지원하지 않는다면(대부분의 엔진이 이를 지원하지 않습니다) 엔진에 설정된 스택 사이즈 제한을 넘었기 때문에 최대 스택 사이즈 초과 에러가 발생합니다.
 
 
 Calculate factorial
@@ -633,6 +684,22 @@ function factorial1() { // Iteration
 function factorial2() { // Recursion
     
 }
+
+팩토리얼은 그 정의에 따라 n!을 n * (n-1)!로 바꿔쓸 수 있습니다.
+
+따라서 factorial(n)의 결과는 n과 factorial(n-1)의 결과를 곱한 값이 되겠죠. 함수의 인수는 n-1에서 1이 될 때까지 점점 줄어들 겁니다.
+
+function factorial(n) {
+  return (n != 1) ? n * factorial(n - 1) : 1;
+}
+
+재귀의 베이스는 1로 잡았는데, 0이어도 상관은 없습니다. 0일 경우 재귀 단계가 하나 더 늘어난다는 것만 다릅니다.
+
+function factorial(n) {
+  return n ? n * factorial(n - 1) : 1;
+}
+
+alert( factorial(5) ); // 120
 
 
 // Fibonacci numbers
@@ -712,7 +779,7 @@ function sum(a, b) {
   return a + b;
 }
 
-alert( sum(1, 2, 3, 4, 5) );
+alert( sum(1, 2, 3, 4, 5) );   // 3
 
 function sumAll(...args) { // args is the name for the array
   let sum = 0;
@@ -808,7 +875,7 @@ console.log( Array.from(str));  //  ["H", "e", "l", "l", "o"]
 // https://javascript.info/closure
 
 // Closure
-let name = "John"; // binding means establishing a relation between a name an a value. 
+let name = "John"; // binding means establishing a relation between a name an a value. 네임과 밸류 사이의 어떤 관계를 만드는 것을 바인딩이라고 함.
 
 function sayHi() {  // Declaration
   console.log("Hi, " + name);
@@ -837,7 +904,7 @@ work(); // what will it show? "Peter" (name where created) or "John" (name where
 
 */
 
-/* JS 80 - Parsing
+/* JS 80 - Parsing (각 문장의 문법적인 구성 또는 구문을 분석하는 과정) 
 
 // Lexical Environment is similar to execution stack(= context) of a function
 // statement and expression . 
@@ -846,21 +913,26 @@ work(); // what will it show? "Peter" (name where created) or "John" (name where
 let name = "John";
 
 function sayHi() {
-  console.log("Hi, " + name);
+  console.log("Hi, " + name);  // body
 }
 
 name = "Peter"; // (*)
 
-sayHi(); // Peter
+sayHi(); // Peter 
+
+// 자바스크립트가 위 식을 읽고 해석하는 과정을 살펴보자.
 
 // one function call makes one execution stack .
-//  JS engine first makes a global context.
+// 한 함수를 실행할 때 하나의 execution stack을 만들어짐.
+//  제일 먼저 만드는 것? => JS engine first makes a global context.
 // Engine collects bindings(=declarations) in its direct scope. 
 // Engine makes LE of the global ==> name, sayHi : parsing
 // Parsing is to make global lexical environment. 
 // Engine again makes LE and [[environment]] of each function or block. 
 
-const list = {lexicalEnvironment: '', [[environment]]: {lexicalEnvironment:'', [[environment]]:{}}} 
+const list = {lexicalEnvironment: '', [[environment]]:{lexicalEnvironment:'', [[environment]]:{value: '', next: ''}}} 
+
+// 렉시칼 엔바이러먼트 안에 바운딩이 있는 지 없는 지 이해해야
 
 */
 
@@ -891,6 +963,7 @@ function User(name) {
 let user = new User("John");
 user.sayHi(); // the method "sayHi" code has access to the outer "name"
 
+
 function makeCounter() {
   let count = 0;
 
@@ -906,6 +979,9 @@ counter() // 0
 counter() // 1
 counter() // 2
 
+// [[Environment]]는 그 둘러싸고 있는 함수(?) 를 말하는 듯.
+  
+// function를 호출하는 순간, 시점에.. 호출하면 그때마다 LE가 구성되는 것임.
 // any new function call makes a brand new LE of it. 
 
 // global LE: makeCounter, counter, [[Environment]]: null
@@ -934,7 +1010,7 @@ counter1(); // 3
 counter2(); // 0 or 4?   -> 0
 */
 
-/* JS 84 - Garbage collection
+/* JS 84 - Garbage collection => 컴퓨터 자원을 아끼는 방법
 // if, while, for makes its own LE.
 for (let i = 0; i < 10; i++) {
   // Each loop has its own Lexical Environment
--- learnJS with J/Advanced JavaScript/chapter2.js
+++ learnJS with J/Advanced JavaScript/chapter2.js
@@ -19,7 +19,7 @@ console.log(sayHi); // sayHi
 
 let user = {
 
-  sayHi() {
+  sayHi() {   // Methods
     // ...
   },
 
@@ -47,7 +47,7 @@ console.log(myFn.name)  // myFn
 
 function f1(a) {}
 function f2(a, b) {}
-function many(a, b, ...more) {}
+function many(a, b, ...more) {}   // rest parameter는 length에 안들어간다.
 
 console.log(f1.length); // 1
 console.log(f2.length); // 2
@@ -59,6 +59,7 @@ console.log(many.length); // 2
 
 
 /* JS 92 - Polymorphism
+// argument의 type이나 length 에 따라서 실행되는 코드내용이 달라지는 것.
 function ask(question, ...handlers) {
   let isYes = confirm(question);  // variables(=bindings) vs. properties. 
 
@@ -115,6 +116,7 @@ myCounter();  // 3
 
 
 /*  JS 94 - A property is not a variable
+// property를 사용..
 function makeCounter() {
   // instead of:
   // let count = 0 // bindings in a functon can't be seen outside the function . 
@@ -122,7 +124,7 @@ function makeCounter() {
   function counter() {
     console.log(counter.count)
     return counter.count++;
-  };
+  };  // property 는 밖에서 볼 수 있음.
 
   counter.count = 0; // property of counter function
 
@@ -136,4 +138,8 @@ counter(); // 2
 counter(); // 3
 counter(); // 4
 counter(); // 5
+
+property는 function의 외부에서도 볼 수 있고 조작할 수도 있다.
+그런데 
+binding(= variable)은 function 외부에서 접근할 수가 없음.
 */
